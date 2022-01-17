d6f94cf0e877d78e123d298ac54bae4f2210058b
Angela Billingsley <abill99@yahoo.com>
2022-01-17 13:10:17 -0500

    Update calculator.js to add business logic math operations and updated scrpts.js and index.html to ad calcultor form and form event handling

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+
+  //$("#doCalc").click(function(){
+  $("form#calc").submit(function(event){
+    event.preventDefault();
+    console.log("calculator id clicked")
+    const number1 = parseInt($("#number1").val());
+    const number2 = parseInt($("#number2").val());
+    let result = -99999999;
+    const operation = parseInt($("#operation").val());
+
+    console.log("operation is " + operation);
+    switch(operation){
+      case 1 : 
+       result = add(number1, number2);
+       break;
+      case 2 :
+        result = subtract(number1, number2);
+        break;
+      case 3 : 
+        result = multiply(number1, number2);
+        break;
+      case 4 :
+        result = divide(number1, number2);
+        break;
+      default:
+        console.log("In case default")
+        result = -99999999;
+
+    }
+   
+    $("#output").text(result);
+
+  });
+
+
+});
